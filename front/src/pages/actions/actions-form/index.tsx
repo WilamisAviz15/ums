5f437c20762a611b46e192ab00330ce12cd7802a
Wilamis Aviz <wmaa@ic.ufal.br>
2023-09-29 09:37:52 -0300

    feat: create actions

--- front/src/pages/actions/actions-form/index.tsx
+++ front/src/pages/actions/actions-form/index.tsx
@@ -1,9 +1,28 @@
-import { Button, TextField } from "@mui/material";
 import React, { useState } from "react";
+import { Button, IconButton, TextField } from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
+
+import styles from "./ActionsForm.module.scss";
+import { useNavigate } from "react-router-dom";
+import actionsService from "../actions.service";
+import { ActionInterface } from "../interfaces/action.interface";
+import { AxiosError } from "axios";
 
 const ActionsForm = () => {
-  const [form, setForm] = useState<any>();
-  const createAction = () => {};
+  const [form, setForm] = useState<ActionInterface>({ name: "" });
+  const navigate = useNavigate();
+
+  const createAction = async () => {
+    try {
+      const res = await actionsService.httpPost(form);
+      navigate(-1);
+      console.log(res);
+    } catch (error: any) {
+      if (error instanceof AxiosError) {
+        // handleOpenSnackbar(error.response?.data["message"], "error");
+      }
+    }
+  };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = event.target;
@@ -15,35 +34,18 @@ const ActionsForm = () => {
 
   return (
     <>
-      <h1 className="title">Criar Evento</h1>
-      <form className="form" onSubmit={createAction}>
+      <div className={styles.title}>
+        <IconButton size="small" color="primary" onClick={() => navigate(-1)}>
+          <ArrowBack />
+        </IconButton>
+        <h1>Ações - criar</h1>
+      </div>
+      <form>
         <div>
-          <TextField
-            className="textField"
-            id="outlined-basic"
-            label="Titulo do Evento"
-            variant="outlined"
-            name="nome"
-            onChange={(v) => handleInputChange(v)}
-          />
-          <TextField
-            className="textField"
-            id="outlined-basic"
-            label="Descrição"
-            variant="outlined"
-            name="descricao"
-            onChange={(v) => handleInputChange(v)}
-          />
-
-          <TextField
-            className="textField"
-            id="outlined-basic"
-            label="Local"
-            variant="outlined"
-            name="local"
-            onChange={(v) => handleInputChange(v)}
-          />
-          <Button title="Criar" />
+          <TextField label="Nome" variant="outlined" name="name" onChange={(v) => handleInputChange(v)} />
+          <Button variant="contained" color="primary" onClick={createAction}>
+            Criar
+          </Button>
         </div>
       </form>
     </>
--- front/src/pages/actions/actions.service.ts
+++ front/src/pages/actions/actions.service.ts
@@ -1,10 +1,16 @@
 import http from "../../shared/services/axios";
+import { ActionInterface } from "./interfaces/action.interface";
 
 class ActionsService {
   constructor() {}
 
   async httpGet(): Promise<any> {
     return await http.get<any, any>("actions/");
+  }
+
+  async httpPost(data: ActionInterface): Promise<{ action: ActionInterface; message: string }> {
+    const response = await http.post<ActionInterface, any>("actions", { data });
+    return response.data;
   }
 }
 
