f5f7b2d6baed71014c7e9684de5775d78736f708
Wilamis Aviz <wmaa@ic.ufal.br>
2024-10-03 20:16:57 -0300

    feat: transform project in LPS library generator

--- api/microsservices/meals/src/modules/meals/interfaces/meal.interface.ts
+++
@@ -1,8 +0,0 @@
-import { SubMealInterface } from '../../../modules/submeals/interfaces/submeal.inteface';
-
-export interface MealInterface {
-  id: number;
-  name: string;
-  price: string;
-  submeals?: SubMealInterface[];
-}
